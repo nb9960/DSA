06b59086893355500b0b6fcc04b6005f17b8e77f
nb9960 <nbodani8@gmail.com>
2020-08-15 21:56:43 +0530

    Added count & quick sort

---
+++ Sorting/count.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include<math.h>
+using namespace std;
+#define MOD 1000000007
+#define ll long long int
+#define fi(i,a,n) for(ll i=a;i<n;i++)
+#define fd(i,a,n) for(ll i=a;i>=n;i--)
+#define pb push_back
+#define fv(i,a) for(auto i:a) 
+#define vi vector<int>
+
+void countSort(ll a[], ll k,ll n, ll min){
+  ll cnt[k];
+  ll output[n];
+  memset(cnt,sizeof(cnt),0);
+  fi(i,0,n){
+    cnt[a[i]-min]++;
+  }
+  fi(i,1,k){
+    cnt[i]+=cnt[i-1];
+  }
+  fd(i,n-1,0){
+    output[cnt[a[i]-min]-1]=a[i];
+    cnt[a[i]-min]--;
+  }
+  fi(i,0,n){
+    a[i]=output[i];
+  }
+}
+
+int main() { 
+  ll n;
+  cin>>n;
+  ll a[n];
+  ll max=INT_MIN,min=INT_MAX;
+  fi(i,0,n){
+    cin>>a[i];
+    if(a[i]>max){
+      max=a[i];
+    }
+    if(a[i]<min){
+      min=a[i];
+    }
+  }
+  ll k=max-min+1;
+  countSort(a,k,n,min);
+  fi(i,0,n){
+    cout<<a[i]<<" ";
+  }
+	return 0; 
+} 
--- Sorting/quick.cpp
+++ Sorting/quick.cpp
@@ -9,6 +9,37 @@ using namespace std;
 #define fv(i,a) for(auto i:a) 
 #define vi vector<int>
 
+ll partition(ll a[],ll low,ll high){ 
+  ll pivot = a[high]; 
+  ll i = low - 1; 
+  fi(j,low,high){
+    if(a[j]<=pivot){
+      i++;
+      swap(a[i],a[j]);
+    }
+  }
+  swap(a[i+1],a[high]);
+  return i+1;
+} 
+
+ll partition_r(ll a[], ll low, ll high){ 
+  // Generate a random number in between 
+  // low .. high 
+  srand(time(NULL)); 
+  ll random = low + rand() % (high - low); 
+  // Swap A[random] with A[high] 
+  swap(a[random], a[high]); 
+  return partition(a, low, high); 
+} 
+
+void quickSort(ll a[], ll low, ll high){ 
+  if (low < high) { 
+    ll q= partition_r(a, low, high); 
+    quickSort(a, low, q); 
+    quickSort(a, q+1, high); 
+  } 
+} 
+
 int main() { 
   ll n;
   cin>>n;
@@ -16,7 +47,7 @@ int main() {
   fi(i,0,n){
     cin>>a[i];
   }
-  
+  quickSort(a,0,n-1);
   fi(i,0,n){
     cout<<a[i]<<" ";
   }
