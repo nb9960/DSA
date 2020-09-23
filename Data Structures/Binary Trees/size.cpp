5693dd9c98ebc69813788e63a92198162fa9c0df
nb9960 <nbodani8@gmail.com>
2020-09-23 19:41:07 +0530

    tree(2)

--- Data Structures/Binary Trees/size.cpp
+++ Data Structures/Binary Trees/size.cpp
@@ -14,6 +14,27 @@ int size(node* node){
   else return(size(node->left) + 1 + size(node->right));
 }
 
+int maxDepth(node* node){
+  if(node==NULL)
+    return 0;
+  else {
+    int l=maxDepth(node->left);
+    int r=maxDepth(node->right);
+    return (max(l,r)+1);
+  }
+}
+
+int diameter(node* node){
+  if(node==NULL){
+    return 0;
+  }
+  int lh=maxDepth(node->left);
+  int rh=maxDepth(node->right);
+  int ld=diameter(node->left);
+  int rd=diameter(node->right);
+  return max(lh+rh+1,max(ld,rd));
+}
+
 node* newNode(int data){
   node* Node=new node();
   Node->data=data;
@@ -30,6 +51,8 @@ int main(){
   root->left->right=newNode(5);
 
   cout<<"Size = "<<size(root)<<endl;
+  cout<<"Height = "<<maxDepth(root)<<endl;
+  cout<<"Diameter = "<<diameter(root)<<endl;
   return 0;
 }
 
